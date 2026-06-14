ed213107ea666b0c5d65b52952b612edfc728e0e
Onkar Pawar <onkarpawar7924@gmail.com>
2026-06-14 18:35:49 +0530

    Missing File

--- src/main/java/com/example/serviceImple/TownFetchServiceImple.java
+++ src/main/java/com/example/serviceImple/TownFetchServiceImple.java
@@ -1,5 +1,21 @@
 package com.example.serviceImple;
 
-public class TownFetchServiceImple {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.entity.Town;
+import com.example.repository.TownRepository;
+import com.example.service.TownFetchService;
+
+@Service
+public class TownFetchServiceImple
+        implements TownFetchService {
+
+    @Autowired
+    private TownRepository repository;
+
+    @Override
+    public Town fetchById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
 }
